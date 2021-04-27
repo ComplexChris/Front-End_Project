63363b6aacfaf6d1a86f1fa56c95f97f4728f347
ComplexChris <ChrisNguyen180@gmail.com>
2021-04-27 12:12:03 -0500

    First commit; adding basic files for project. Includes sudo-code in the scripts.js file.

---
+++ Script.js
@@ -0,0 +1,8 @@
+// Create a Dropzone object
+// Capture path and store data of file locally
+
+// Use Pantry ( https://getpantry.cloud/apiv1/pantry/809cff74-8863-45df-a7b8-44641613bcf8/basket/testBasket )
+// Use that to create and store variables from GoFile
+
+// Submit another Post request to Pantry with update object
+// {fileName:[path, apiKey, ID]}
